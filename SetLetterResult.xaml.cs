4ae2e20298e86fe4c7cb8c9df1f25a7b8cf0eecc
Ed Korsberg <eckorsberg@ra.rockwell.com>
2024-04-18 18:09:33 -0400

    remove extraneous code

--- SetLetterResult.xaml.cs
+++ SetLetterResult.xaml.cs
@@ -20,76 +20,28 @@ namespace WordleSover
     /// </summary>
     public partial class SetLetterResult : UserControl
     {
-
-#if true
-        //LetterStateEnum SetLetterRadioResult;
-       // LetterState letterState;
-
         public SetLetterResult()
         {
             //this.letterState = letter;
             InitializeComponent();
         }
 
-        
-
-        private void SetLetterOkClick(object sender, RoutedEventArgs e)
-        {
-            //if (Match.IsChecked == true) { SetLetterRadioResult = LetterStateEnum.eMatch; }
-            //else if (ValidWrongSpot.IsChecked == true) { SetLetterRadioResult = LetterStateEnum.eValidWrongSpot; }
-            //else if (InvalidLetter.IsChecked == true) { SetLetterRadioResult = LetterStateEnum.eInvalid; }
-
-            // do not exit unless choice is made
-            if ((Match.IsChecked == true) || (ValidWrongSpot.IsChecked == true) || ( InvalidLetter.IsChecked == true))
-            {
-                Window parentWindow = (Window)this.Parent;
-                //Window.GetWindow(this).DialogResult = true;
-                parentWindow.Close();
-            }
-        }
-
         private void Match_Checked(object sender, RoutedEventArgs e)
         {
-            Match.IsChecked = true;
             Window parentWindow = (Window)this.Parent;
             parentWindow.Close();
         }
 
         private void ValidWrongSpot_Checked(object sender, RoutedEventArgs e)
         {
-            ValidWrongSpot.IsChecked = true;
             Window parentWindow = (Window)this.Parent;
             parentWindow.Close();
         }
 
         private void InvalidLetter_Checked(object sender, RoutedEventArgs e)
         {
-            InvalidLetter.IsChecked = true;
             Window parentWindow = (Window)this.Parent;
             parentWindow.Close();
         }
-
-#else
-        //public int SetLetterRadioResult = 0;  //1=Match, 2=ValidWrongSpot, 3=Invalid
-        LetterStateEnum SetLetterRadioResult = LetterStateEnum.eUnknown;  //1=Match, 2=ValidWrongSpot, 3=Invalid
-        //public SetLetterResult(ref LetterState result)
-        public SetLetterResult()
-        {
-            //SetLetterRadioResult = result;
-            InitializeComponent();
-        }
-
-        private void SetLetterOkClick(object sender, RoutedEventArgs e)
-        {
-            if (Match.IsChecked == true)                { SetLetterRadioResult = LetterStateEnum.eMatch; }
-            else if (ValidWrongSpot.IsChecked == true)  { SetLetterRadioResult = LetterStateEnum.eValidWrongSpot; }
-            else if (InvalidLetter.IsChecked == true)   { SetLetterRadioResult = LetterStateEnum.eInvalid; }
-
-            Window parentWindow = (Window)this.Parent;
-            //Window.GetWindow(this).DialogResult = true;
-            parentWindow.Close();
-        }
-
-#endif
     }
 }
